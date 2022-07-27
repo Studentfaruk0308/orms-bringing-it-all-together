2c412c421c0b44bfb50972d9ee010f242f517161
faruk <studentfaruk0308@gmail.com>
2022-07-27 20:03:47 +1000

    After Completion

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,9 @@
 require 'sqlite3'
+
 require_relative '../lib/dog'
 
 DB = {:conn => SQLite3::Database.new("db/dogs.db")}
+
+# require_relative '/home/faruk/Development/Code/Phase-3/orms-bringing-it-all-together/lib/dog'
+
+# DB = {:conn => SQLite3::Database.new("/home/faruk/Development/Code/Phase-3/orms-bringing-it-all-together/db/dogs.db")}
